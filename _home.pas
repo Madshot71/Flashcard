831d4675d33e21c00b5e6fb91033c7ce359eecd8
GhostBoy <Ghosthlongwane360@gmail.com>
2025-09-28 17:08:50 +0200

    new file:   __history/_admin.dfm.~1~ 	new file:   __history/_admin.dfm.~2~ 	new file:   __history/_admin.dfm.~3~ 	new file:   __history/_admin.pas.~1~ 	new file:   __history/_admin.pas.~2~ 	new file:   __history/_admin.pas.~3~ 	new file:   __history/_home.dfm.~3~ 	new file:   __history/_home.dfm.~4~ 	new file:   __history/_home.dfm.~5~ 	new file:   __history/_home.pas.~3~ 	new file:   __history/_home.pas.~4~ 	new file:   __history/_home.pas.~5~ 	new file:   __history/_login.dfm.~3~ 	new file:   __history/_login.dfm.~4~ 	new file:   __history/_login.dfm.~5~ 	new file:   __history/_login.pas.~3~ 	new file:   __history/_login.pas.~4~ 	new file:   __history/_login.pas.~5~ 	modified:   _admin.dcu 	modified:   _admin.dfm 	modified:   _admin.pas 	modified:   _home.dcu 	modified:   _home.dfm 	modified:   _home.pas 	modified:   _login.dcu 	modified:   _login.dfm 	modified:   _login.pas

--- _home.pas
+++ _home.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs;
+  Dialogs, StdCtrls, pngimage, jpeg, ExtCtrls, Buttons;
 
 type
   THome = class(TForm)
+    Background: TImage;
+    TransperantBackground: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Image1: TImage;
+    ScrollBox1: TScrollBox;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    Label3: TLabel;
   private
     { Private declarations }
   public
